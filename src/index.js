68e1bcfe87f244e5d90fa8d3c00b0e52db56e8e2
adeyinka oluwaseun <seun.adeyinka@gmail.com>
2020-10-15 23:42:05 +0200

    using props to pass data to a component

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function Hello() {
+function Hello(props) {
+  console.log(props);
+  console.log(Object.keys(props)); // returns an array of keys from the props
   return (
     <div>
-      <h1> Welcome to React</h1>
-      <p>Let's us build</p>
+      <h1> Welcome to {props.name}</h1>
+      <p>Let's us build and {props.message}</p>
+      <p>{props.number}</p>
+      <p>{Object.keys(props).length} Props Total</p>
     </div>
   );
 }
 
-ReactDOM.render(<Hello />, document.getElementById("root"));
+ReactDOM.render(
+  <Hello name="Seun" message="Have fun!" number={4} />,
+  document.getElementById("root")
+);
