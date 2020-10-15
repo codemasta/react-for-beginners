744f29ab9967767e8489988bf9fb198d35fca140
adeyinka oluwaseun <seun.adeyinka@gmail.com>
2020-10-15 23:31:28 +0200

    create first and simple react component

--- src/index.js
+++ src/index.js
@@ -2,30 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-let city = "Madrid";
-let person = {
-  name: "Seun",
-  country: "Spain",
-};
+function Hello() {
+  return (
+    <div>
+      <h1> Welcome to React</h1>
+      <p>Let's us build</p>
+    </div>
+  );
+}
 
-ReactDOM.render(
-  //React.createElement("h1", { style: { color: "red" } }, "Hello!"),
-  <div>
-    <h1 id="heading" className="cool-text">
-      {" "}
-      Hello from {city}
-    </h1>
-    <h1>
-      {" "}
-      You are {person.name} from {person.country}
-    </h1>
-    <ul>
-      <li>Hot Dog</li>
-      <li>Hot Yam</li>
-      <li>Hot Bread</li>
-      <li>Hot Corn</li>
-      <li>Hot Beans</li>
-    </ul>
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<Hello />, document.getElementById("root"));
