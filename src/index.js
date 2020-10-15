b27afc97376932e0339ef46e7e5e8c68e2eca5a5
adeyinka oluwaseun <seun.adeyinka@gmail.com>
2020-10-15 23:44:13 +0200

    passing data to react component as seperate values

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function Hello(props) {
-  console.log(props);
-  console.log(Object.keys(props)); // returns an array of keys from the props
+function Hello({ name, message, number }) {
   return (
     <div>
-      <h1> Welcome to {props.name}</h1>
-      <p>Let's us build and {props.message}</p>
-      <p>{props.number}</p>
-      <p>{Object.keys(props).length} Props Total</p>
+      <h1> Welcome to {name}</h1>
+      <p>Let's us build and {message}</p>
+      <p>{number} Props Total</p>
     </div>
   );
 }
